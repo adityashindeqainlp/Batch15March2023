cbe8fcd376028d78587c58c77a2d7557d6cd09e6
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-06-05 13:23:46 -0400

    Change in default values of global variable

--- Batch15March2023/src/CoreJava/P9_DefaultValuesGlobalVariable.java
+++ Batch15March2023/src/CoreJava/P9_DefaultValuesGlobalVariable.java
@@ -5,9 +5,27 @@ package CoreJava;
  */
 public class P9_DefaultValuesGlobalVariable {
 
+// Complier provides same values for static and non static global variable 	
+	public static byte a;
+	public static short b;
+	public static int c;
+	public static long d;
+	public static float e;
+	public static double f;
+	public static boolean g;
+	public static char h;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		System.out.println("Defualt Value of Byte : " + a);
+		System.out.println("Default Value of Short : " + b);
+		System.out.println("Default Value of Integer : " + c);
+		System.out.println("Default Value of Long : " + d);
+		System.out.println("Default Value of Float : " + e);
+		System.out.println("Default Value of Double : " + f);
+		System.out.println("Default Value of boolean : " + g);
+		System.out.println("Default Value of Char : " + h);
 
+	}
 }
