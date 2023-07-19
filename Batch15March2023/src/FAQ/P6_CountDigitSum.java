3594c17fee49f610560ffe8e0899ccd6e986dd51
Aditya Shinde <Aditya Shinde@INFOSEUM33>
2023-07-19 12:51:48 -0400

    FAQ

---
+++ Batch15March2023/src/FAQ/P6_CountDigitSum.java
@@ -0,0 +1,22 @@
+package FAQ;
+
+public class P6_CountDigitSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int num = 1234;
+		int sum = 0;
+		
+		while(num>0) {
+			
+		sum = sum  + num%10 ;
+		num = num/10;
+		
+		}
+
+		System.out.println(sum);
+	}
+
+}
